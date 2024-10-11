43758b713b1a7425f548b4fd0e6d84845457edb1
Pakapol Taubol <88127599+PakapolTaubol@users.noreply.github.com>
2024-10-11 23:57:23 +0700

    fixed confilc

--- lib/main.dart
+++ lib/main.dart
@@ -38,28 +38,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
   File? selectedMedia;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
@@ -90,7 +72,6 @@ class _MyHomePageState extends State<MyHomePage> {
         tooltip: 'Select Image',
         child: const Icon(Icons.add),
       ),
-      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
@@ -133,61 +114,7 @@ class _MyHomePageState extends State<MyHomePage> {
     final RecognizedText recognizedText =
         await textRecognizer.processImage(inputimage);
     String text = recognizedText.text;
-    LineSplitter ls = new LineSplitter();
-    List<String> textList = ls.convert(text);
-    for (var i = 0; i < textList.length; i++) {
-      if (textList[i].contains("THB")) {
-        log("text: $textList[i]");
-        text = textList[i];
-        textRecognizer.close();
-        return text;
-      }
-    }
-    textRecognizer.close();
-
-    return text;
-  }
-
-  Widget _imageView() {
-    if (selectedMedia == null) {
-      return const Center(
-        child: Text("Select image"),
-      );
-    }
-    return Center(
-      child: Image.file(
-        selectedMedia!,
-        width: 200,
-      ),
-    );
-  }
-
-  Widget _extractTextView() {
-    if (selectedMedia == null) {
-      return const Center(
-        child: Text("No Result"),
-      );
-    }
-    return FutureBuilder(
-        future: _extractText(selectedMedia!),
-        builder: (context, snapshot) {
-          return Text(
-            snapshot.data ?? "",
-            style: const TextStyle(fontSize: 50),
-          );
-        });
-  }
-
-  Future<String?> _extractText(File file) async {
-    final textRecognizer = TextRecognizer(
-      script: TextRecognitionScript.latin,
-    );
-
-    final InputImage inputimage = InputImage.fromFile(file);
-    final RecognizedText recognizedText =
-        await textRecognizer.processImage(inputimage);
-    String text = recognizedText.text;
-    LineSplitter ls = new LineSplitter();
+    LineSplitter ls = LineSplitter();
     List<String> textList = ls.convert(text);
     for (var i = 0; i < textList.length; i++) {
       if (textList[i].contains("THB")) {
